a08beb5f4f1895071a126266be86dcf55d3e597d
CraigQA147 <143518785+CraigQA147@users.noreply.github.com>
2023-08-30 13:12:21 +0100

    changed back end url

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -2,7 +2,8 @@ angular.module('app').factory('ConfigService', [function() {
 
     var service = {};
 
-    service.apiBase = 'http://localhost:8001/';
+    //service.apiBase = 'http://localhost:8001/';
+    service.apiBase = "http://quiz-api-xyz.eu-north-1.elasticbeanstalk.com/";
 
     return service;
 }]);
